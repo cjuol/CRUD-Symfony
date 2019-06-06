b41b0f06dfdf741f0d52391c158ee4ca81c3fe84
Cristóbal Jurado Oller <c.jurado.oller@gmail.com>
2019-06-07 00:50:11 +0200

    Generada estructura Symfony con  clase Usuario

---
+++ README.md
@@ -0,0 +1,40 @@
+# CRUD + Gestión de usuarios
+> Repositorio creado para facilitar una implementación de gestión de usuarios en otros proyectos Symfony.
+
+
+[![NPM Version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+[![Downloads Stats][npm-downloads]][npm-url]
+
+
+![](../header.png)
+
+## Instalación
+
+Para la instalación utilizaremos composer ya que facilita el trabajo. Debemos entrar en el directorio del repositorio y ejecutar el comandod de instalación
+
+```sh
+composer install
+```
+
+Para el despliegue es necesario disponer de PHP instalado y una vez ejecutado el comando anterior se deberá editar el fichero parameters.yml para ajustar los parametros a los de nuestros servidores.
+
+
+## Historial de versiones
+
+* 0.0.1
+    * CAMBIO: Modificado el directorio "README.md" con una plantilla.
+* 0.2.0
+    * CAMBIO: Modificada la plantilla del "README.md"
+
+
+## Meta
+
+
+[https://github.com/yourname/github-link](https://github.com/cjuol/)
+
+[npm-image]: https://img.shields.io/npm/v/datadog-metrics.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/datadog-metrics
+[npm-downloads]: https://img.shields.io/npm/dm/datadog-metrics.svg?style=flat-square
+[travis-image]: https://img.shields.io/travis/dbader/node-datadog-metrics/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/dbader/node-datadog-metrics
---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UsuarioRepository")
+ */
+class Usuario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Clave", type="string", length=255)
+     */
+    private $clave;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Correo", type="string", length=255)
+     */
+    private $correo;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get clave
+     *
+     * @return string
+     */
+    public function getClave()
+    {
+        return $this->clave;
+    }
+
+    /**
+     * Set clave
+     *
+     * @param string $clave
+     *
+     * @return Usuario
+     */
+    public function setClave($clave)
+    {
+        $this->clave = $clave;
+
+        return $this;
+    }
+
+    /**
+     * Get correo
+     *
+     * @return string
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * Set correo
+     *
+     * @param string $correo
+     *
+     * @return Usuario
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+}
+
